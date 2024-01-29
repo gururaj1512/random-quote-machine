2f2a36e36cdbc35dd6dcd9db42065dfbb3a3747a
gururaj1512 <gururajgurram1512@gmail.com>
2024-01-29 11:43:48 +0530

    Commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Quotes from './Components/quotes.json';
+import Twitter from './twitter.svg';
+
+const getRandomColor = () => {
+  const red = Math.floor(Math.random()*128);
+  const blue = Math.floor(Math.random()*128);
+  const green = Math.floor(Math.random()*128);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+}
 
 function App() {
+
+  const [articles, setArticles] = useState(Quotes.quotes[0].quote)
+  const [author, setAuthor] = useState(Quotes.quotes[0].author)
+  const [randomColor, setRandomColor] = useState(getRandomColor());
+
+  let quoteNumber = Math.floor(Math.random() * Quotes.quotes.length);
+  let changeQuote = () => {
+    quoteNumber++
+    console.log(quoteNumber);
+    setArticles(Quotes.quotes[quoteNumber].quote)
+    setAuthor(Quotes.quotes[quoteNumber].author)
+    setRandomColor(getRandomColor())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: randomColor}}>
+      <div className="quotebox" id="quote-box">
+        <div className="text" id="text" style={{color: randomColor}}>{articles}</div>
+        <div className="author" id="author" style={{color: randomColor}}>-{author}</div>
+        <div id='abcd'>
+          <div id="new-quote">
+            <button className="btn btn-primary" onClick={changeQuote} style={{backgroundColor: randomColor}}>New Quote</button>
+          </div>
+          <div id="tweet-quote" style={{backgroundColor: randomColor}}>
+            <a href="." target='_blank'><img src={Twitter} alt="" id='img' /></a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
